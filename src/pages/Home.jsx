6c0ce9b10c4b9c0b9de17df06cbf048221d79f01
Ratinhonhonho <r.romano.silva5@gmail.com>
2026-04-11 16:29:10 -0300

    feat: montando estrutura inicial da home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-function Home(){
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+import Header from '../components/Header';
+import CountryCard from '../components/CountryCard';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <section>
+          <h2>Lista de Países</h2>
+          <p>Busca, filtro e paginação virão aqui.</p>
+        </section>
+
+        <section>
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
