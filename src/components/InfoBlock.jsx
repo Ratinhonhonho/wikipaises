3788dbcf17e4e01838ee200379f495775918fb66
Ratinhonhonho <r.romano.silva5@gmail.com>
2026-04-11 16:44:33 -0300

    feat: montando os detalhes do Detail

---
+++ src/components/InfoBlock.jsx
@@ -0,0 +1,9 @@
+function InfoBlock() {
+  return (
+    <div>
+      <p>Informação</p>
+    </div>
+  );
+}
+
+export default InfoBlock;
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,40 @@
-function Detail(){
-    return(
-        <div>
-            <h1>Detail</h1>
-        </div>
-    );
+import InfoBlock from '../components/InfoBlock';
+
+function Detail() {
+  return (
+    <div>
+      <h1>Detalhes do País</h1>
+
+      <section>
+        <img
+          src="https://flagcdn.com/w320/br.png"
+          alt="Bandeira do país"
+          width="250"
+        />
+      </section>
+
+      <section>
+        <h2>Brasil</h2>
+        <p>República Federativa do Brasil</p>
+      </section>
+
+      <section>
+        <InfoBlock />
+        <InfoBlock />
+        <InfoBlock />
+        <InfoBlock />
+      </section>
+
+      <section>
+        <h3>Sobre o país</h3>
+        <p>
+          Aqui ficará uma descrição estática ou mockada sobre o país.
+        </p>
+      </section>
+
+      <button>Voltar para a lista</button>
+    </div>
+  );
 }
 
 export default Detail;
