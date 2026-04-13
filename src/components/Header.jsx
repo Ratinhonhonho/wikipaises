4afb6a56983782e46e4fa05215c59ea04a0eebde
Ratinhonhonho <r.romano.silva5@gmail.com>
2026-04-13 19:51:36 -0300

    feat: melhorando a Header e adcionando um icone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
+import { Globe } from 'lucide-react';
 import styles from './Header.module.css';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>WikiPaíses</h1>
+      <div className={styles.content}>
+        <div className={styles.titleRow}>
+          <Globe size={30} className={styles.icon} />
+          <h1 className={styles.title}>WikiPaíses</h1>
+        </div>
+
+        <p className={styles.subtitle}>
+          Explore informações sobre países de forma simples e organizada.
+        </p>
+      </div>
     </header>
   );
 }
